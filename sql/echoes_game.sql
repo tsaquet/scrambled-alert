2e5566c91044c7c18e3b73b41ceadec608bc77d5
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2011-08-15 22:24:22 +0200

    FPO : Correxions de la structure de la BDD

--- sql/echoes_game.sql
+++ sql/echoes_game.sql
@@ -1,19 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.0
+-- version 3.3.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Lun 15 Août 2011 à 22:04
--- Version du serveur: 5.1.44
--- Version de PHP: 5.2.13
+-- Généré le : Lun 15 Août 2011 à 22:19
+-- Version du serveur: 5.1.54
+-- Version de PHP: 5.3.5-1ubuntu7.2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données: `echoes_game`
@@ -22,10 +16,10 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `achievements`
+-- Structure de la table `ACHIEVEMENTS`
 --
 
-CREATE TABLE IF NOT EXISTS `achievements` (
+CREATE TABLE IF NOT EXISTS `ACHIEVEMENTS` (
   `ACH_ID` int(20) NOT NULL AUTO_INCREMENT,
   `ACH_NAME` varchar(20) NOT NULL,
   `ACH_DESC` varchar(50) NOT NULL,
@@ -33,62 +27,67 @@ CREATE TABLE IF NOT EXISTS `achievements` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
--- Contenu de la table `achievements`
+-- Contenu de la table `ACHIEVEMENTS`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `achievements_users`
+-- Structure de la table `ACHIEVEMENTS_USERS`
 --
 
-CREATE TABLE IF NOT EXISTS `achievements_users` (
+CREATE TABLE IF NOT EXISTS `ACHIEVEMENTS_USERS` (
   `ASR_USR_ID` int(20) NOT NULL,
   `ASR_ACH_ID` int(20) NOT NULL,
   UNIQUE KEY `ASR_USR_ID` (`ASR_USR_ID`,`ASR_ACH_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `achievements_users`
+-- Contenu de la table `ACHIEVEMENTS_USERS`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `scores_best`
+-- Structure de la table `SCORES_BEST`
 --
 
-CREATE TABLE IF NOT EXISTS `scores_best` (
-  `SBE_USR_ID` int(20) NOT NULL COMMENT 'fk USERS',
+CREATE TABLE IF NOT EXISTS `SCORES_BEST` (
+  `SBE_USR_ID` bigint(20) NOT NULL COMMENT 'fk USERS',
   `SBE_LEVEL` varchar(20) NOT NULL COMMENT 'level name',
   `SBE_SCORE` int(20) NOT NULL COMMENT 'player best score for the level',
   `SBE_NB_CLIC` int(10) NOT NULL COMMENT 'nb clic for best score',
   `SBE_PERCENT_SATISF` int(3) NOT NULL COMMENT 'percent for best score',
   `SBE_NB_PLAYED` int(10) NOT NULL COMMENT 'nb played total',
   `SBE_NB_WIN` int(10) NOT NULL COMMENT 'nb win total',
+  UNIQUE KEY `IDX_ID_LEVEL` (`SBE_USR_ID`,`SBE_LEVEL`),
   KEY `SBE_USR_ID` (`SBE_USR_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `scores_best`
+-- Contenu de la table `SCORES_BEST`
 --
 
-INSERT INTO `scores_best` (`SBE_USR_ID`, `SBE_LEVEL`, `SBE_SCORE`, `SBE_NB_CLIC`, `SBE_PERCENT_SATISF`, `SBE_NB_PLAYED`, `SBE_NB_WIN`) VALUES
+INSERT INTO `SCORES_BEST` (`SBE_USR_ID`, `SBE_LEVEL`, `SBE_SCORE`, `SBE_NB_CLIC`, `SBE_PERCENT_SATISF`, `SBE_NB_PLAYED`, `SBE_NB_WIN`) VALUES
 (559788374, 'Novice', 2820, 14, 100, 22, 5),
 (559788374, 'Normal', 11200, 0, 80, 22, 1),
 (559788374, 'Expert', 0, 0, 0, 0, 0),
-(559788374, 'Maître', 0, 0, 0, 0, 0);
+(559788374, 'Maître', 0, 0, 0, 0, 0),
+(100000831236752, 'Novice', 0, 0, 0, 0, 0),
+(100000831236752, 'Normal', 0, 0, 0, 0, 0),
+(100000831236752, 'Expert', 0, 0, 0, 0, 0),
+(100000831236752, 'Maître', 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `scores_race`
+-- Structure de la table `SCORES_RACE`
 --
 
-CREATE TABLE IF NOT EXISTS `scores_race` (
-  `SRA_USR_ID` int(20) NOT NULL COMMENT 'fk USERS',
+CREATE TABLE IF NOT EXISTS `SCORES_RACE` (
+  `SRA_USR_ID` bigint(20) NOT NULL COMMENT 'fk USERS',
   `SRA_SCORE` int(20) NOT NULL COMMENT 'cumul score',
   `SRA_AVERAGE_NB_CLIC` int(20) NOT NULL COMMENT 'average nb clic',
   `SRA_MOST_PLAYED_LEVEL` varchar(20) NOT NULL COMMENT 'most played level',
@@ -96,20 +95,21 @@ CREATE TABLE IF NOT EXISTS `scores_race` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `scores_race`
+-- Contenu de la table `SCORES_RACE`
 --
 
-INSERT INTO `scores_race` (`SRA_USR_ID`, `SRA_SCORE`, `SRA_AVERAGE_NB_CLIC`, `SRA_MOST_PLAYED_LEVEL`) VALUES
-(559788374, 65380, 0, '1');
+INSERT INTO `SCORES_RACE` (`SRA_USR_ID`, `SRA_SCORE`, `SRA_AVERAGE_NB_CLIC`, `SRA_MOST_PLAYED_LEVEL`) VALUES
+(559788374, 65380, 0, '1'),
+(100000831236752, 0, 0, '');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Structure de la table `USERS`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `USR_ID` int(20) NOT NULL COMMENT 'id fb',
+CREATE TABLE IF NOT EXISTS `USERS` (
+  `USR_ID` bigint(20) NOT NULL COMMENT 'id fb',
   `USR_NAME` varchar(100) NOT NULL COMMENT 'name fb',
   `USR_FIRST_NAME` varchar(100) NOT NULL COMMENT 'first name fb',
   `USR_LAST_NAME` varchar(100) NOT NULL COMMENT 'last name fb',
@@ -118,9 +118,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `users`
+-- Contenu de la table `USERS`
 --
 
-INSERT INTO `users` (`USR_ID`, `USR_NAME`, `USR_FIRST_NAME`, `USR_LAST_NAME`, `USR_USERNAME`) VALUES
+INSERT INTO `USERS` (`USR_ID`, `USR_NAME`, `USR_FIRST_NAME`, `USR_LAST_NAME`, `USR_USERNAME`) VALUES
 (559788374, 'Thomas Saquet', 'Thomas', 'Saquet', 'thomas.saquet'),
-(0, 'Guest', 'Guest', 'Guest', 'Guest');
+(0, 'Guest', 'Guest', 'Guest', 'Guest'),
+(100000831236752, 'Florent Poinsaut', 'Florent', 'Poinsaut', 'florent.poinsaut');
